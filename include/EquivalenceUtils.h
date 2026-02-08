ad2a0b06a521aaa7d779468be4bb9b891836abc9
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-02-08 17:21:22 +0100

    restructure EquivalenceSelectGreedy

--- include/EquivalenceUtils.h
+++ include/EquivalenceUtils.h
@@ -1,6 +1,8 @@
 #ifndef EQUIVALENCE_UTILS_H
 #define EQUIVALENCE_UTILS_H
 
+#include "EquivalenceDialect.h"
+
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Support/LogicalResult.h"
 
@@ -11,6 +13,11 @@ namespace mlir::equivalence {
 /// Returns success if the transformation was successful.
 LogicalResult insertGraphInFunction(func::FuncOp funcOp,
                                     bool insertSingleElementEqs);
+
+/// Run greedy cost-based selection on a single GraphOp.
+/// Assigns min_cost_index attributes to ClassOps based on minimum-cost
+/// operands.
+void selectGreedy(GraphOp graphOp, int64_t defaultCost);
 
 } // namespace mlir::equivalence
 
