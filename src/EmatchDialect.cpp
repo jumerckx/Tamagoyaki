63db7aed9d6d5c87fd97f746555c75ba38c6bc49
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-03-23 12:01:40 +0100

    simplify

--- src/EmatchDialect.cpp
+++ src/EmatchDialect.cpp
@@ -95,11 +95,6 @@ namespace {
 
 struct PendingMatch {
   Operation *op;
-  mlir::detail::PDLByteCode::MatchResult matchResult;
-};
-
-struct PendingMultiMatch {
-  Operation *op;
   mlir::detail::MultiMatcherPDLByteCode::MatchResult matchResult;
 };
 
@@ -145,7 +140,6 @@ bool runSaturation(MLIRContext *ctx, PDLPatternModule pdlPattern,
                    ModuleOp irModule, int maxIters, int maxNodes,
                    RewriterBase::Listener *listener) {
   TAMAGOYAKI_SCOPED_TIMER("runSaturation");
-  RewritePatternSet patternList(ctx);
 
   ClassOpUnionFind uf{};
   HashConsPatternRewriter hashconsRewriter(ctx);
@@ -208,187 +202,89 @@ bool runSaturation(MLIRContext *ctx, PDLPatternModule pdlPattern,
         return op;
       });
 
-  // Count top-level pdl_interp.func ops to decide single vs multi matcher.
   ModuleOp pdlModule = pdlPattern.getModule();
-  unsigned numMatcherFuncs = 0;
-  for (Operation &op : pdlModule.getBody()->getOperations())
-    if (isa<pdl_interp::FuncOp>(&op))
-      numMatcherFuncs++;
+  auto configs = pdlPattern.takeConfigs();
+  auto configMap = pdlPattern.takeConfigMap();
+  auto constraintFns = pdlPattern.takeConstraintFunctions();
+  auto rewriteFns = pdlPattern.takeRewriteFunctions();
 
-  if (numMatcherFuncs > 1) {
-    // --- Multi-matcher path ---
-    auto configs = pdlPattern.takeConfigs();
-    auto configMap = pdlPattern.takeConfigMap();
-    auto constraintFns = pdlPattern.takeConstraintFunctions();
-    auto rewriteFns = pdlPattern.takeRewriteFunctions();
+  mlir::detail::MultiMatcherPDLByteCode bytecode(
+      pdlModule, std::move(configs), configMap, std::move(constraintFns),
+      std::move(rewriteFns));
 
-    mlir::detail::MultiMatcherPDLByteCode multiBytecode(
-        pdlModule, std::move(configs), configMap, std::move(constraintFns),
-        std::move(rewriteFns));
+  mlir::detail::MultiMatcherMutableState bytecodeState;
+  SmallVector<PendingMatch> allMatches;
 
-    mlir::detail::MultiMatcherMutableState multiState;
-    SmallVector<PendingMultiMatch> allMatches;
-
-    int nIters = 0;
-    bool maxNodesExceeded = false;
-    while (true) {
-      TAMAGOYAKI_SCOPED_TIMER("iteration " + std::to_string(nIters + 1));
-      LLVM_DEBUG({
-        irModule.walk([&](equivalence::GraphOp graph) {
-          int classes = 0;
-          int nodes = 0;
-          graph.walk([&](Operation *op) {
-            if (dyn_cast<equivalence::ClassOp>(op)) {
-              classes += 1;
-            } else {
-              nodes += op->getNumResults();
-              for (auto result : op->getResults()) {
-                if (!(result.hasOneUse() &&
-                      dyn_cast<equivalence::ClassOp>(*result.user_begin()))) {
-                  classes += 1;
-                }
+  int nIters = 0;
+  bool maxNodesExceeded = false;
+  while (true) {
+    TAMAGOYAKI_SCOPED_TIMER("iteration " + std::to_string(nIters + 1));
+    LLVM_DEBUG({
+      irModule.walk([&](equivalence::GraphOp graph) {
+        int classes = 0;
+        int nodes = 0;
+        graph.walk([&](Operation *op) {
+          if (dyn_cast<equivalence::ClassOp>(op)) {
+            classes += 1;
+          } else {
+            nodes += op->getNumResults();
+            for (auto result : op->getResults()) {
+              if (!(result.hasOneUse() &&
+                    dyn_cast<equivalence::ClassOp>(*result.user_begin()))) {
+                classes += 1;
               }
             }
-          });
-          llvm::dbgs() << "Graph has " << classes << " e-classes and " << nodes
-                       << " e-nodes (iteration " << nIters << ").\n";
-        });
-      });
-
-      nIters++;
-      if (nIters > maxIters)
-        break;
-      LLVM_DEBUG(llvm::dbgs() << "Equality saturation: starting iteration "
-                              << nIters << "\n");
-
-      multiBytecode.initializeMutableState(multiState);
-
-      {
-        TAMAGOYAKI_SCOPED_TIMER("match");
-        irModule.walk([&](Operation *op) {
-          auto dialect = op->getDialect();
-          if (dialect != nullptr &&
-              isa<equivalence::EquivalenceDialect>(dialect))
-            return;
-
-          SmallVector<mlir::detail::MultiMatcherPDLByteCode::MatchResult>
-              opMatches;
-          multiBytecode.match(op, hashconsRewriter, opMatches, multiState);
-
-          for (auto &match : opMatches)
-            allMatches.push_back({op, std::move(match)});
-        });
-      }
-      {
-        TAMAGOYAKI_SCOPED_TIMER("rewrite");
-        for (const auto &pm : allMatches) {
-          hashconsRewriter.setInsertionPoint(pm.op);
-          (void)multiBytecode.rewrite(hashconsRewriter, pm.matchResult,
-                                      multiState);
-          if (maxNodes > 0 &&
-              hashconsRewriter.getNodeCount() > (uint64_t)maxNodes) {
-            LLVM_DEBUG(llvm::dbgs() << "Node limit exceeded: "
-                                    << hashconsRewriter.getNodeCount() << " > "
-                                    << maxNodes << "\n");
-            maxNodesExceeded = true;
-            break;
           }
-        }
-        allMatches.clear();
-        multiState.cleanupAfterMatchAndRewrite();
-      }
-
-      bool didRebuild = uf.rebuild(hashconsRewriter);
-      if (maxNodesExceeded || !didRebuild)
-        break;
-    }
-  } else {
-    // --- Single-matcher path (original) ---
-    patternList.add(std::move(pdlPattern));
-
-    FrozenRewritePatternSet frozenPatterns(std::move(patternList));
-
-    SmallVector<PendingMatch> allMatches;
-
-    const auto *bytecode = frozenPatterns.getPDLByteCode();
-    if (!bytecode)
-      return false;
-
-    mlir::detail::PDLByteCodeMutableState bytecodeState;
-
-    int nIters = 0;
-    bool maxNodesExceeded = false;
-    while (true) {
-      TAMAGOYAKI_SCOPED_TIMER("iteration " + std::to_string(nIters + 1));
-      LLVM_DEBUG({
-        irModule.walk([&](equivalence::GraphOp graph) {
-          int classes = 0;
-          int nodes = 0;
-          graph.walk([&](Operation *op) {
-            if (dyn_cast<equivalence::ClassOp>(op)) {
-              classes += 1;
-            } else {
-              nodes += op->getNumResults();
-              for (auto result : op->getResults()) {
-                if (!(result.hasOneUse() &&
-                      dyn_cast<equivalence::ClassOp>(*result.user_begin()))) {
-                  classes += 1;
-                }
-              }
-            }
-          });
-          llvm::dbgs() << "Graph has " << classes << " e-classes and " << nodes
-                       << " e-nodes (iteration " << nIters << ").\n";
         });
+        llvm::dbgs() << "Graph has " << classes << " e-classes and " << nodes
+                     << " e-nodes (iteration " << nIters << ").\n";
       });
+    });
 
-      nIters++;
-      if (nIters > maxIters)
-        break;
-      LLVM_DEBUG(llvm::dbgs() << "Equality saturation: starting iteration "
-                              << nIters << "\n");
+    nIters++;
+    if (nIters > maxIters)
+      break;
+    LLVM_DEBUG(llvm::dbgs()
+               << "Equality saturation: starting iteration " << nIters << "\n");
 
-      bytecode->initializeMutableState(bytecodeState);
+    bytecode.initializeMutableState(bytecodeState);
 
-      {
-        TAMAGOYAKI_SCOPED_TIMER("match");
-        irModule.walk([&](Operation *op) {
-          auto dialect = op->getDialect();
-          if (dialect != nullptr &&
-              isa<equivalence::EquivalenceDialect>(dialect))
-            return;
+    {
+      TAMAGOYAKI_SCOPED_TIMER("match");
+      irModule.walk([&](Operation *op) {
+        auto dialect = op->getDialect();
+        if (dialect != nullptr && isa<equivalence::EquivalenceDialect>(dialect))
+          return;
 
-          SmallVector<mlir::detail::PDLByteCode::MatchResult, 4> opMatches;
+        SmallVector<mlir::detail::MultiMatcherPDLByteCode::MatchResult>
+            opMatches;
+        bytecode.match(op, hashconsRewriter, opMatches, bytecodeState);
 
-          bytecode->match(op, hashconsRewriter, opMatches, bytecodeState);
-
-          for (auto &match : opMatches)
-            allMatches.push_back({op, std::move(match)});
-        });
-      }
-      {
-        TAMAGOYAKI_SCOPED_TIMER("rewrite");
-        for (const auto &pm : allMatches) {
-          hashconsRewriter.setInsertionPoint(pm.op);
-          (void)bytecode->rewrite(hashconsRewriter, pm.matchResult,
-                                  bytecodeState);
-          if (maxNodes > 0 &&
-              hashconsRewriter.getNodeCount() > (uint64_t)maxNodes) {
-            LLVM_DEBUG(llvm::dbgs() << "Node limit exceeded: "
-                                    << hashconsRewriter.getNodeCount() << " > "
-                                    << maxNodes << "\n");
-            maxNodesExceeded = true;
-            break;
-          }
-        }
-        allMatches.clear();
-        bytecodeState.cleanupAfterMatchAndRewrite();
-      }
-
-      bool didRebuild = uf.rebuild(hashconsRewriter);
-      if (maxNodesExceeded || !didRebuild)
-        break;
+        for (auto &match : opMatches)
+          allMatches.push_back({op, std::move(match)});
+      });
     }
+    {
+      TAMAGOYAKI_SCOPED_TIMER("rewrite");
+      for (const auto &pm : allMatches) {
+        hashconsRewriter.setInsertionPoint(pm.op);
+        (void)bytecode.rewrite(hashconsRewriter, pm.matchResult, bytecodeState);
+        if (maxNodes > 0 &&
+            hashconsRewriter.getNodeCount() > (uint64_t)maxNodes) {
+          LLVM_DEBUG(llvm::dbgs() << "Node limit exceeded: "
+                                  << hashconsRewriter.getNodeCount() << " > "
+                                  << maxNodes << "\n");
+          maxNodesExceeded = true;
+          break;
+        }
+      }
+      allMatches.clear();
+      bytecodeState.cleanupAfterMatchAndRewrite();
+    }
+
+    bool didRebuild = uf.rebuild(hashconsRewriter);
+    if (maxNodesExceeded || !didRebuild)
+      break;
   }
 
   return true;
